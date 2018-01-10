09ce4aa8d4811779c22572d94380ca1f53dbf94c
Markus Ahrweiler <markus.ahrweiler@web.de>
2018-01-10 20:39:25 +0100

    Initial commit

---
+++ src/wtf/mephiztopheles/defered/Defered.java
@@ -0,0 +1,98 @@
+package wtf.mephiztopheles.defered;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Defered<RESOLVE, REJECT, PROGRESS> {
+
+    private final Promise<RESOLVE, REJECT, PROGRESS> promise;
+
+    public Defered() {
+        promise = new Promise();
+    }
+
+    public void progress(PROGRESS argument) {
+
+        if (!promise.state.equals(Promise.State.PENDING))
+            throw new IllegalStateException(promise.state.message);
+
+        promise.progress.forEach((callback) -> {
+            callback.call(argument);
+        });
+    }
+
+    public void resolve(RESOLVE argument) {
+
+        if (!promise.state.equals(Promise.State.PENDING))
+            throw new IllegalStateException(promise.state.message);
+
+        promise.resolve.forEach((callback) -> {
+            callback.call(argument);
+        });
+
+        promise.state = Promise.State.RESOLVED;
+    }
+
+    public void reject(REJECT argument) {
+
+        if (!promise.state.equals(Promise.State.PENDING))
+            throw new IllegalStateException(promise.state.message);
+
+        promise.reject.forEach((callback) -> {
+            callback.call(argument);
+        });
+
+        promise.state = Promise.State.REJECTED;
+    }
+
+    public Promise<RESOLVE, REJECT, PROGRESS> promise() {
+        return promise;
+    }
+
+    public static class Promise<RESOLVE, REJECT, PROGRESS> {
+
+        private List<Callback<RESOLVE>> resolve = new ArrayList<>();
+        private List<Callback<REJECT>> reject = new ArrayList<>();
+        private List<Callback<PROGRESS>> progress = new ArrayList<>();
+        private State state = State.PENDING;
+
+        public State state() {
+            return state;
+        }
+
+        public Promise<RESOLVE, REJECT, PROGRESS> then(Callback resolve, Callback reject, Callback progress) {
+
+            if (resolve != null)
+                this.resolve.add(resolve);
+
+            if (reject != null)
+                this.reject.add(reject);
+
+            if (progress != null)
+                this.progress.add(progress);
+
+            return this;
+        }
+
+        public Promise<RESOLVE, REJECT, PROGRESS> then(Callback resolve, Callback reject) {
+            return then(resolve, reject, null);
+        }
+
+        public Promise<RESOLVE, REJECT, PROGRESS> then(Callback resolve) {
+            return then(resolve, null, null);
+        }
+
+        public static enum State {
+
+            PENDING(""),
+            REJECTED("The Promise was already rejected"),
+            RESOLVED("The Promise was already resolved");
+
+            public final String message;
+
+            State(String message) {
+                this.message = message;
+            }
+        }
+    }
+}
---
+++ src/wtf/mephiztopheles/defered/test/Test.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wtf.mephiztopheles.defered.test;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import wtf.mephiztopheles.defered.Callback;
+import wtf.mephiztopheles.defered.Defered;
+import wtf.mephiztopheles.defered.Defered.Promise;
+
+/**
+ *
+ * @author marku
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Callback<String> callback = (Callback<String>) (String argument) -> {
+            System.out.println(argument);
+        };
+        doWork().then(callback, (Callback<Integer>) (Integer argument) -> {
+            System.out.println(argument);
+        }, callback);
+    }
+
+    public static Promise<String, Integer, String> doWork() {
+
+        Defered<String, Integer, String> defer = new Defered();
+
+        Timer timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+
+                int randomNum = ThreadLocalRandom.current().nextInt(2, 6);
+
+                try {
+
+                    for (int i = 0; i < randomNum; i++) {
+                        defer.progress("I'm working on it...");
+                        Thread.sleep(200);
+                    }
+
+                    if (randomNum % 2 == 0)
+                        defer.resolve("It works");
+                    else
+                        defer.reject(randomNum);
+
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }, 500);
+
+        return defer.promise();
+    }
+}
